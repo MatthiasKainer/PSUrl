8832020408d55d4e78ead72cb272045bf577b998
Matthias Kainer <mkainer@autoscout24.com>
2014-03-27 10:01:39 +0100

    Credentials can now be stored

--- PSUrl.psm1
+++ PSUrl.psm1
@@ -1,4 +1,15 @@
+register PSCredentialStore;
+
 $webclient = New-Object System.Net.WebClient;
+
+function Get-BaseUrlAndPort {
+	param (
+		[parameter(Mandatory = $true)]
+		[string]$url
+	);
+	$uri = new-object System.Uri($url.ToLower());
+	"$($uri.Scheme)$($uri.SchemeDelimiter)$($uri.Host):$($uri.Port)";
+}
 
 function Set-UrlCredentials {
 	param (
@@ -9,12 +20,27 @@ function Set-UrlCredentials {
 	$webclient.Credentials = $credentials;
 }
 
+function Set-UrlCredentialsFor {
+	param (
+		[parameter(Mandatory = $true)]
+		[string]$url
+	);
+	
+	Add-CredentialsToStore(Get-BaseUrlAndPort($url));
+}
+
 function Get-UrlContent
 {
 	param (
 		[parameter(Mandatory = $true)]
 		[Uri]$url
 	);
+	
+	$baseUrl = Get-BaseUrlAndPort($url);
+	if(In-CredentialStore($baseUrl)) {
+		$webclient.Credentials = Get-CredentialStore($baseUrl);
+	}
+	
 	if ($url.HostNameType -ne [System.UriHostNameType]::Dns) {
 		throw "Invalid url type";
 	}
@@ -31,6 +57,10 @@ function Add-UrlContent {
 		throw "Invalid url type";
 	}
 	
+	$baseUrl = Get-BaseUrlAndPort($url);
+	if(In-CredentialStore($baseUrl)) {
+		$webclient.Credentials = Get-CredentialStore($baseUrl);
+	}
 	$webclient.Headers[[System.Net.HttpRequestHeader]::ContentType] = "application/x-www-form-urlencoded";
 	return $webclient.UploadData($url, "PUT", [System.Text.Encoding]::ASCII.GetBytes($data));
 }
@@ -44,11 +74,16 @@ function Set-UrlContent {
 		throw "Invalid url type";
 	}
 	
+	$baseUrl = Get-BaseUrlAndPort($url);
+	if(In-CredentialStore($baseUrl)) {
+		$webclient.Credentials = Get-CredentialStore($baseUrl);
+	}
 	$webclient.Headers[[System.Net.HttpRequestHeader]::ContentType] = "application/x-www-form-urlencoded";
 	return $webclient.UploadData($url, "POST", [System.Text.Encoding]::ASCII.GetBytes($data));
 }
 
 Export-ModuleMember -function Set-UrlCredentials;
+Export-ModuleMember -function Set-UrlCredentialsFor;
 Export-ModuleMember -function Get-UrlContent;
 Export-ModuleMember -function Add-UrlContent;
 Export-ModuleMember -function Set-UrlContent;
